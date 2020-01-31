44b4529b7db6781b8d9eabafde42a106a581348a
adhee <adheenaus@gmail.com>
2020-01-31 10:14:49 -0800

    Add project files.

---
+++ FindGoodString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindGoodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
